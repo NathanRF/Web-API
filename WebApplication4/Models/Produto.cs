995a720d759edf0b7fca2f9936b3c9bb37627b81
Nathan R.F.P <nathanseribeiro@hotmail.com>
2019-05-13 15:55:26 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication4/Models/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication4.Models
+{
+    public class Produto
+    {
+        [Key]
+        public string Nome { get; set; }
+
+        public Decimal Preco { get; set; }
+    }
+}
---
+++ WebApplication4/Models/ProdutosContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using WebApplication4.Models;
+
+namespace WebApplication4
+{
+    public class ProdutosContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                new System.Data.SqlClient.SqlConnectionStringBuilder()
+                {
+                    DataSource = "NATHANSE-LAPTOP",
+                    InitialCatalog = "Produtos",
+                    UserID = "sa",
+                    Password = "12345"
+                }.ConnectionString
+
+                );            
+        }
+    }
+}
